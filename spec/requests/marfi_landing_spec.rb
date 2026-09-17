0a4da5be5fef6e63d6a3b1a396ca7980d6633423
Danny Mehditash <danny@marfi.io>
2026-09-17 09:23:41 -0700

    Seed an owner for the installed-portal landing regression

--- spec/requests/marfi_landing_spec.rb
+++ spec/requests/marfi_landing_spec.rb
@@ -4,6 +4,8 @@ require 'rails_helper'
 
 RSpec.describe 'MARFI landing page', type: :request do
   it 'shows the branded entry point with sign-in, attribution and corresponding source' do
+    create(:user)
+
     get '/'
 
     expect(response).to have_http_status(:ok)
