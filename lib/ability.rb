1add19e65db65fcf119fd3dddc7778c9c14fac8a
Danny Mehditash <danny@marfi.io>
2026-09-17 00:53:53 -0700

    Refactor role authorization setup

--- lib/ability.rb
+++ lib/ability.rb
@@ -4,45 +4,64 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    account_scope = { account_id: user.account_id }
-
     case user.role
-    when User::VIEWER_ROLE
-      can :read, Template, account_scope
-      can :read, TemplateFolder, account_scope
-      can :read, TemplateSharing, template: account_scope
-      can :read, Submission, account_scope
-      can :read, Submitter, account_scope
-      can :read, Account, id: user.account_id
-    when User::EDITOR_ROLE
-      can %i[read create update], Template, account_scope
-      can :destroy, Template, account_scope
-      can :manage, TemplateFolder, account_scope
-      can :manage, TemplateSharing, template: account_scope
-      can :manage, Submission, account_scope
-      can :manage, Submitter, account_scope
-      can :read, Account, id: user.account_id
-    else
-      can %i[read create update], Template, Abilities::TemplateConditions.collection(user) do |template|
-        Abilities::TemplateConditions.entity(template, user:, ability: 'manage')
-      end
-
-      can :destroy, Template, account_scope
-      can :manage, TemplateFolder, account_scope
-      can :manage, TemplateSharing, template: account_scope
-      can :manage, Submission, account_scope
-      can :manage, Submitter, account_scope
-      can :manage, User, account_scope
-      can :manage, EncryptedConfig, account_scope
-      can :manage, EncryptedUserConfig, user_id: user.id
-      can :manage, AccountConfig, account_scope
-      can :manage, UserConfig, user_id: user.id
-      can :manage, Account, id: user.account_id
-      can :manage, AccessToken, user_id: user.id
-      can :manage, McpToken, user_id: user.id
-      can :manage, WebhookUrl, account_scope
-
-      can :manage, :mcp
+    when User::VIEWER_ROLE then configure_viewer(user)
+    when User::EDITOR_ROLE then configure_editor(user)
+    else configure_admin(user)
     end
+  end
+
+  private
+
+  def account_scope(user)
+    { account_id: user.account_id }
+  end
+
+  def configure_viewer(user)
+    scope = account_scope(user)
+
+    can :read, Template, scope
+    can :read, TemplateFolder, scope
+    can :read, TemplateSharing, template: scope
+    can :read, Submission, scope
+    can :read, Submitter, scope
+    can :read, Account, id: user.account_id
+  end
+
+  def configure_editor(user)
+    scope = account_scope(user)
+
+    can %i[read create update], Template, scope
+    can :destroy, Template, scope
+    can :manage, TemplateFolder, scope
+    can :manage, TemplateSharing, template: scope
+    can :manage, Submission, scope
+    can :manage, Submitter, scope
+    can :read, Account, id: user.account_id
+  end
+
+  def configure_admin(user)
+    scope = account_scope(user)
+
+    can %i[read create update], Template, Abilities::TemplateConditions.collection(user) do |template|
+      Abilities::TemplateConditions.entity(template, user:, ability: 'manage')
+    end
+
+    can :destroy, Template, scope
+    can :manage, TemplateFolder, scope
+    can :manage, TemplateSharing, template: scope
+    can :manage, Submission, scope
+    can :manage, Submitter, scope
+    can :manage, User, scope
+    can :manage, EncryptedConfig, scope
+    can :manage, EncryptedUserConfig, user_id: user.id
+    can :manage, AccountConfig, scope
+    can :manage, UserConfig, user_id: user.id
+    can :manage, Account, id: user.account_id
+    can :manage, AccessToken, user_id: user.id
+    can :manage, McpToken, user_id: user.id
+    can :manage, WebhookUrl, scope
+
+    can :manage, :mcp
   end
 end
